73d01690a2369ab02900d9c9248e7b992599c842
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2024-09-10 00:02:45 +0300

    sa

--- tests/dags/bash_operatoe.py
+++ tests/dags/bash_operatoe.py
@@ -5,5 +5,5 @@ from airflow.operators.bash import BashOperator
 
 DEFAULT_DATE = datetime(2019, 12, 1)
 
-dag = DAG(dag_id='test_dag_under_subdir2', start_date=DEFAULT_DATE, schedule_interval=None)
+dag = DAG(dag_id='bash_operator', start_date=DEFAULT_DATE, schedule_interval=None)
 task = BashOperator(task_id='task1', bash_command='hostname', dag=dag)
